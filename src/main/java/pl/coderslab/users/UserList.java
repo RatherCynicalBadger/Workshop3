b0ed8fcbec321a6de2a481a40e2ce1944ca8fb4a
Błażej Konieczny <blazej.k936@gmail.com>
2024-02-29 20:55:22 +0100

    DB connection works

--- src/main/java/pl/coderslab/users/UserList.java
+++ src/main/java/pl/coderslab/users/UserList.java
@@ -1,19 +1,33 @@
 package pl.coderslab.users;
 
+import pl.coderslab.entity.User;
+import pl.coderslab.entity.UserDAO;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.ArrayList;
 
 @WebServlet(value = "/user/list")
 public class UserList extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        HttpSession session = request.getSession();
+        UserDAO userDAO = new UserDAO();
+
+        ArrayList<User> users = userDAO.findAll();
+
+        session.setAttribute("userDAO", userDAO);
+        session.setAttribute("users", users);
+
+        getServletContext().getRequestDispatcher("/users/list.jsp").forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        //implement add,show,edit,delete
     }
 }
--- src/main/java/pl/coderslab/utils/DBUtil.java
+++ src/main/java/pl/coderslab/utils/DBUtil.java
@@ -11,16 +11,13 @@ public class DBUtil {
     private static DataSource dataSource;
     public static Connection getConnection() throws SQLException {
         return getInstance().getConnection();   }
-
     private static DataSource getInstance() {
         if (dataSource == null) {
             try {
                 Context initContext = new InitialContext();
                 Context envContext = (Context)initContext.lookup("java:/comp/env");
                 dataSource = (DataSource)envContext.lookup("jdbc/users");
-            } catch (NamingException e) {
-                System.out.println(e.getMessage());
-            }
+            } catch (NamingException e) { e.printStackTrace(); }
         }
         return dataSource;
     }
